91edc5a38016e42dd1140542bc34921cabe1ec97
Aanu Olakunle <olakunleaanu@gmail.com>
2021-03-06 08:35:48 +0100

    Complete adding pin on map when longpress

--- Virtual Tourist/Controller/MapViewController.swift
+++ Virtual Tourist/Controller/MapViewController.swift
@@ -9,7 +9,9 @@ import UIKit
 import MapKit
 
 class MapViewController: UIViewController{
-
+    
+    // MARK: IBOutlets
+    
     @IBOutlet weak var touristMapView: MKMapView!
     
     override func viewDidLoad() {
@@ -17,9 +19,30 @@ class MapViewController: UIViewController{
         configureMap()
     }
     
-    func configureMap(){
+    override func viewWillAppear(_ animated: Bool) {
         navigationController?.navigationBar.isHidden = true
+    }
+    
+    func configureMap(){
+       
+        // add a gesture recognizer to the map
+        let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(onLongPress))
+        touristMapView.addGestureRecognizer(longPressRecognizer)
+        
         setRegion()
+    }
+    
+    
+    @objc func onLongPress(sender: UIGestureRecognizer) {
+        if (sender.state == .began){
+            let location = sender.location(in: touristMapView)
+            let locationOnMap = touristMapView.convert(location, toCoordinateFrom: touristMapView)
+            
+            // To add anotation
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = locationOnMap
+            self.touristMapView.addAnnotation(annotation)
+        }
     }
     
     // set map region (center coordinate and the span)
@@ -42,6 +65,13 @@ class MapViewController: UIViewController{
         let data: [Double] = [center.latitude, center.longitude, span.latitudeDelta, span.longitudeDelta]
         UserDefaults.standard.setValue(data, forKey: Constants.UserDefaults.region)
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if(segue.identifier == Constants.SegueIdentifier.mapViewToPhotoView){
+            let destination = segue.destination as! PhotoAlbumViewController
+            destination.coordinate = sender as? CLLocationCoordinate2D
+        }
+    }
 
 
 }
@@ -50,6 +80,26 @@ class MapViewController: UIViewController{
 extension MapViewController: MKMapViewDelegate {
     func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
         saveRegion(region: mapView.region)
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard annotation is MKPointAnnotation else { return nil }
+
+        let identifier = "pin"
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        } else {
+            pinView?.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        performSegue(withIdentifier: Constants.SegueIdentifier.mapViewToPhotoView, sender: view.annotation?.coordinate)
+        mapView.deselectAnnotation(view.annotation, animated: true)
     }
 }
 
--- Virtual Tourist/Controller/PhotoAlbumViewController.swift
+++ Virtual Tourist/Controller/PhotoAlbumViewController.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
+import MapKit
 
 class PhotoAlbumViewController: UIViewController {
+    
+    // MARK: IBOutlets
+
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var coordinate: CLLocationCoordinate2D!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configure()
     }
     
+    func configure(){
+        navigationController?.navigationBar.isHidden = false
+        configureMapView()
+    }
+    
+    func configureMapView(){
+        mapView.centerCoordinate = coordinate
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        mapView.addAnnotation(annotation)
+    }
+}
 
 
+extension PhotoAlbumViewController : MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard annotation is MKPointAnnotation else { return nil }
+
+        let identifier = "pin"
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        } else {
+            pinView?.annotation = annotation
+        }
+        
+        return pinView
+    }
+}
+
+extension PhotoAlbumViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+   
 }
